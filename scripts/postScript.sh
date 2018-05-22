2ddf22073fb49b61651382f2a5fc750f2851a204
Saso Sabotin <sasos90@gmail.com>
2018-05-22 22:06:17 +0200

    Post install script adapter for creating .env file

--- scripts/postScript.sh
+++ scripts/postScript.sh
@@ -1,2 +1,6 @@
+# Create directory and file for building Android version.
 mkdir -p android/app/src/main/assets
 touch android/app/src/main/assets/index.android.bundle
+
+# Copy .env.dev file in order to get environment variables correctly.
+cp -n .env.dev .env
